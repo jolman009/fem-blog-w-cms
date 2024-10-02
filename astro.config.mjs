997b2c376079e73466ce25714f122fc2ba4d735e
Joel Guzman <jolman009@yahoo.com>
2024-10-01 22:34:00 -0500

    tt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
-import sentry from '@sentry/astro';
-
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [sentry(), preact(), tailwind()]
-});
-
-
-
+export default defineConfig({});
